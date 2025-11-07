cd645ec599074b600e70607d8ad298039bf59dcc
BattiFratto <battistonfilippo3@gmail.com>
2025-11-07 12:54:09 +0100

    provaprova3

--- src/main/java/it/university/MainApp.java
+++ src/main/java/it/university/MainApp.java
@@ -6,6 +6,8 @@ import it.university.service.*;
 
 //BRUTTO ANATROCCOLO
 
+//EGLKSSLMGSLGJ
+
 public class MainApp {
     public static void main(String[] args) {
         StudentService studentService = new StudentService();
